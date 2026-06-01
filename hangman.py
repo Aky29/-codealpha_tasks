c421479ab9388e3bbabc4c72170967b10ae4d720
Aky29 <me.anishkumaryadav@gmail.com>
2026-06-02 02:17:34 +0530

    the win condition is implemented but there is still a problem because it is case sensitive

--- hangman.py
+++ hangman.py
@@ -1,10 +1,17 @@
 secret_word = input("Enter the secret word : ")
-for _ in range(5):
+guesses = 5
+won = False
+while guesses >= 0:
     user_guess = input("Guess the word : ")
+    if user_guess == secret_word:
+        print(user_guess)
+        won = True
+        break
     for i in range(len(user_guess)):
         if user_guess[i] == secret_word[i]:
             print(user_guess[i],end="")
         else:
             print("_",end = "")
     print("")
-print("game finished")
+    guesses-=1
+print("You Won") if won  else print("You Lose")
